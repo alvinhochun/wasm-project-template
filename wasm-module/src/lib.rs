13b16c024737a5c914a7baa646343b482ecfb398
Alvin Wong <alvinhochun@gmail.com>
2020-06-16 21:27:13 +0800

    Make demo with Rhai

--- wasm-module/src/lib.rs
+++ wasm-module/src/lib.rs
@@ -1,6 +1,22 @@
 use wasm_bindgen::prelude::*;
 
+mod scripting;
+
 #[wasm_bindgen]
-pub extern "C" fn add(x: u32, y: u32) -> u32 {
-    x + y
+pub extern "C" fn run_script(
+    script: String,
+    print_callback: js_sys::Function,
+    debug_callback: js_sys::Function,
+) -> Result<String, JsValue> {
+    console_error_panic_hook::set_once();
+
+    Ok(scripting::run_script(
+        &script,
+        move |s| {
+            let _ = print_callback.call1(&JsValue::null(), &JsValue::from_str(s));
+        },
+        move |s| {
+            let _ = debug_callback.call1(&JsValue::null(), &JsValue::from_str(s));
+        },
+    )?)
 }
---
+++ wasm-module/src/scripting.rs
@@ -0,0 +1,21 @@
+use rhai::packages::{ArithmeticPackage, EvalPackage, LogicPackage, Package};
+
+pub fn run_script(
+    script: &str,
+    print_callback: impl Fn(&str) + 'static,
+    debug_callback: impl Fn(&str) + 'static,
+) -> Result<String, String> {
+    let engine = {
+        let mut engine = rhai::Engine::new();
+        engine.load_package(ArithmeticPackage::new().get());
+        engine.load_package(LogicPackage::new().get());
+        engine.load_package(EvalPackage::new().get());
+        engine.on_print(move |s| print_callback(s));
+        engine.on_debug(move |s| debug_callback(s));
+        engine
+    };
+    let script_ast = engine.compile(&script).map_err(|e| e.to_string())?;
+
+    let result: rhai::Dynamic = engine.eval_ast(&script_ast).map_err(|e| e.to_string())?;
+    Ok(result.to_string())
+}
